37ae43f0e652e4f8b0d9993053b9f76161ff5040
= <beckyljones151192@gmail.com>
2021-11-04 18:46:40 +0000

    added recommended by to food.js

--- src/food.js
+++ src/food.js
@@ -2,18 +2,22 @@ let food = [
   {
     food: "Deep fried Mars bar",
     isDone: false,
+    // recommendedBy: "Max bootcamp4"
   },
   {
     food: "US Biscuits",
     isDone: false,
+    // recommendedBy: "Liz.K SoC Coach"
   },
   {
     food: "Sushi",
     isDone: false,
+    // recommendedBy: "Emma bootcamp4"
   },
   {
     food: "Tofu",
     isDone: false,
+    // recommendedBy: "Amy B13 Technology"
   },
 ];
 
