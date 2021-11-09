52ae04c7c4a6760c223676317132912bae1fc34c
= <beckyljones151192@gmail.com>
2021-11-09 16:24:42 +0000

    some nav changes as  nav size is wider than screen

--- src/components/Nav/nav.js
+++ src/components/Nav/nav.js
@@ -8,52 +8,47 @@ import Home from "../Home/home";
 function Nav() {
   return (
     <Router>
-      <div>
-        {/* <h3>hi</h3> */}
+      {/* <div> */}
+      {/* <h3>hi</h3> */}
 
-        <ReactBootStrap.Navbar
-          //   collapseOnSelect
-          //   expand="sm"
-          bg="light"
-          variant="light"
-        >
-          {/* <nav> */}
-          {/* <Link to="/">
+      <ReactBootStrap.Navbar
+        //   collapseOnSelect
+        //   expand="sm"
+        bg="light"
+        variant="light"
+        //   style={{ justifyContent: "space-between" }}
+      >
+        {/* <nav> */}
+        {/* <Link to="/">
             <ReactBootStrap.Navbar.Brand>Home</ReactBootStrap.Navbar.Brand>
           </Link> */}
-          {/* &nbsp; */}
-          {/* <ReactBootStrap.Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        {/* &nbsp; */}
+        {/* <ReactBootStrap.Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <ReactBootStrap.Navbar.Collapse id="responsive-navbar-nav"> */}
-          <ReactBootStrap.Nav className="mr_auto">
-            {/* <Link to="/food"> */}
-            <ReactBootStrap.Nav.Link href="/food">Eat</ReactBootStrap.Nav.Link>
-            {/* </Link> */}
-            <ReactBootStrap.Nav.Link href="/city">
-              Visit
-            </ReactBootStrap.Nav.Link>
-            <ReactBootStrap.Nav.Link href="/country">
-              Stay
-            </ReactBootStrap.Nav.Link>
-            <ReactBootStrap.Nav.Link href="/book">Read</ReactBootStrap.Nav.Link>
-            <ReactBootStrap.Nav.Link href="/tele">
-              Binge
-            </ReactBootStrap.Nav.Link>
-            <ReactBootStrap.Nav.Link href="/film">
-              Watch
-            </ReactBootStrap.Nav.Link>
-          </ReactBootStrap.Nav>
-          {/* </ReactBootStrap.Navbar.Collapse> */}
-          {/* </nav> */}
-        </ReactBootStrap.Navbar>
-        <Routes>
-          <Route path="/food" element={<Food />} />
-          {/* <Food />
+        <ReactBootStrap.Nav variant="tabs" className="justify-content-end">
+          {/* <Link to="/food"> */}
+          <ReactBootStrap.Nav.Link href="/food">Eat</ReactBootStrap.Nav.Link>
+          {/* </Link> */}
+          <ReactBootStrap.Nav.Link href="/city">Visit</ReactBootStrap.Nav.Link>
+          <ReactBootStrap.Nav.Link href="/country">
+            Stay
+          </ReactBootStrap.Nav.Link>
+          <ReactBootStrap.Nav.Link href="/book">Read</ReactBootStrap.Nav.Link>
+          <ReactBootStrap.Nav.Link href="/tele">Binge</ReactBootStrap.Nav.Link>
+          <ReactBootStrap.Nav.Link href="/film">Watch</ReactBootStrap.Nav.Link>
+        </ReactBootStrap.Nav>
+        {/* </ReactBootStrap.Navbar.Collapse> */}
+        {/* </nav> */}
+      </ReactBootStrap.Navbar>
+      <Routes>
+        <Route path="/food" element={<Food />} />
+        {/* <Food />
           </Route> */}
-          <Route path="/" element={<Home />} />
-          {/* <Home />
+        <Route path="/" element={<Home />} />
+        {/* <Home />
           </Route> */}
-        </Routes>
-      </div>
+      </Routes>
+      {/* </div> */}
     </Router>
   );
 }
